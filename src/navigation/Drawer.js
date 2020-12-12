1aa0068942dc33b52775f5ad3c6c4fcec5d2157c
abdohema2016 <abdelrhman.ibrahim101@gmail.com>
2020-12-12 19:51:21 +0200

    add button to open drawer

--- src/navigation/Drawer.js
+++ src/navigation/Drawer.js
@@ -1,18 +1,36 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, TouchableOpacity, Image} from 'react-native';
 import {createDrawerNavigator} from '@react-navigation/drawer';
+import {createStackNavigator} from '@react-navigation/stack';
 import Home from '../screens/Home';
 import About from '../screens/About';
 import Settings from '../screens/Settings';
 
 const Drawer = createDrawerNavigator();
+const Stack = createStackNavigator();
 
+const Screens = ({navigation}) => {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerTransparent: true,
+        headerTitle: null,
+        headerLeft: () => (
+          <TouchableOpacity onPress={navigation.openDrawer}>
+            <Text>Menu</Text>
+          </TouchableOpacity>
+        ),
+      }}>
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="About" component={About} />
+      <Stack.Screen name="Settings" component={Settings} />
+    </Stack.Navigator>
+  );
+};
 export default () => {
   return (
     <Drawer.Navigator initialRouteName="Home">
-      <Drawer.Screen name="Home" component={Home} />
-      <Drawer.Screen name="About" component={About} />
-      <Drawer.Screen name="Settings" component={Settings} />
+      <Drawer.Screen name="Screens" component={Screens} />
     </Drawer.Navigator>
   );
 };
