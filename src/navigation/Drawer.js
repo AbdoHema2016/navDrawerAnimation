a616c2e550ef17c702a3e6ddddf1b5eda252281a
abdohema2016 <abdelrhman.ibrahim101@gmail.com>
2020-12-12 20:25:46 +0200

    add animated view to drawer

--- src/navigation/Drawer.js
+++ src/navigation/Drawer.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import {View, Text, TouchableOpacity, Image} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, TouchableOpacity, Image, StyleSheet} from 'react-native';
 import {
   createDrawerNavigator,
   DrawerContentScrollView,
@@ -9,6 +9,7 @@ import {createStackNavigator} from '@react-navigation/stack';
 import Home from '../screens/Home';
 import About from '../screens/About';
 import Settings from '../screens/Settings';
+import Animated from 'react-native-reanimated';
 
 const Drawer = createDrawerNavigator();
 const Stack = createStackNavigator();
@@ -31,30 +32,44 @@ const DrawerContent = (props) => {
     </DrawerContentScrollView>
   );
 };
-const Screens = ({navigation}) => {
+const Screens = ({navigation, style}) => {
   return (
-    <Stack.Navigator
-      screenOptions={{
-        headerTransparent: true,
-        headerTitle: null,
-        headerLeft: () => (
-          <TouchableOpacity onPress={navigation.openDrawer}>
-            <Text>Menu</Text>
-          </TouchableOpacity>
-        ),
-      }}>
-      <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="About" component={About} />
-      <Stack.Screen name="Settings" component={Settings} />
-    </Stack.Navigator>
+    <Animated.View style={[{flex: 1}, style]}>
+      <Stack.Navigator
+        screenOptions={{
+          headerTransparent: true,
+          headerTitle: null,
+          headerLeft: () => (
+            <TouchableOpacity onPress={navigation.openDrawer}>
+              <Text>Menu</Text>
+            </TouchableOpacity>
+          ),
+        }}>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="About" component={About} />
+        <Stack.Screen name="Settings" component={Settings} />
+      </Stack.Navigator>
+    </Animated.View>
   );
 };
 export default () => {
+  const [progress, setProgress] = useState(new Animated.Value(0));
+  const scale = Animated.interpolate(progress, {
+    inputRange: [0, 1],
+    outputRange: [1, 0.8],
+  });
+  const screenStyles = {transform: [{scale}]};
   return (
     <Drawer.Navigator
       initialRouteName="Home"
-      drawerContent={(props) => <DrawerContent {...props} />}>
-      <Drawer.Screen name="Screens" component={Screens} />
+      drawerContent={(props) => {
+        setProgress(props.progress);
+        return <DrawerContent {...props} />;
+      }}>
+      <Drawer.Screen name="Screens">
+        {(props) => <Screens {...props} style={screenStyles} />}
+      </Drawer.Screen>
     </Drawer.Navigator>
   );
 };
+const styles = StyleSheet.create({});
